34e221de67875470170745dd82c522754d34d958
Puput Puspita <puputpuspita@student.telkomuniversity.ac.id>
2024-04-11 21:59:30 +0700

    fix conflicting files

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,14 +2,25 @@ import { Navigate, RouteObject } from "react-router-dom";
 import Login from "pages/login";
 import EventCalendarPage from "pages/event-calendar/index.pages";
 import DashboardLayout from "layout/dashboard";
+import PostPage, { postRouteName } from "pages/post/index.page";
+import DetailPostPage, { detailPostRouteName } from "pages/post/detail.page";
+import CreatePost, { createPostRouteName } from "pages/post/create.page";
+import CreateComment, {
+  createCommentRouteName,
+} from "pages/comment/create.page";
+
 
 const protectedRoutes: RouteObject[] = [
-  { path: "*", element: <Navigate to="/event" /> },
+  { path: "*", element: <Navigate to="/post" /> },
   {
     path: "",
     element: <DashboardLayout />,
     children: [
       { path: "not-found", element: <>Page Not Found</> },
+      { path: postRouteName, element: <PostPage /> },
+      { path: detailPostRouteName, element: <DetailPostPage /> },
+      { path: createPostRouteName, element: <CreatePost /> },
+      { path: createCommentRouteName, element: <CreateComment /> },
       { path: "/event", element: <EventCalendarPage /> },
     ],
   },
